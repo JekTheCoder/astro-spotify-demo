50679d17178b8630a93411213dd51ce232c854e4
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-03-05 21:19:07 -0500

    feat | can forwad next/back songs

--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -9,6 +9,20 @@ import { Pause, Play } from "./solid-buttons";
 import Volume, { type VolumeData } from "./MediaPlayer/Volume";
 import SongPlayed from "./SongPlayed";
 
+const NextIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 20 20"
+  >
+    <path
+      fill="currentColor"
+      d="M12.244 9.52L5.041 4.571C4.469 4.188 4 4.469 4 5.196v9.609c0 .725.469 1.006 1.041.625l7.203-4.951s.279-.199.279-.478c0-.28-.279-.481-.279-.481M14 4h1c.553 0 1 .048 1 .6v10.8c0 .552-.447.6-1 .6h-1c-.553 0-1-.048-1-.6V4.6c0-.552.447-.6 1-.6"
+    />
+  </svg>
+);
+
 export default function MediaPlayer() {
   const musicStore = useStore(useMusicPlayed);
   const volumeSignal = createSignal<VolumeData>({
@@ -18,10 +32,12 @@ export default function MediaPlayer() {
 
   const audio = (<audio src=""></audio>) as HTMLAudioElement;
 
-  const toggle = () => musicStore().toggle();
   const disabled = () => musicStore().data === null;
   const playing = () => Boolean(musicStore().data?.isPlaying);
   const song = () => musicStore().data?.song;
+
+  const toggle = () => musicStore().toggle();
+	const stepSong = (step: number) => musicStore().stepSong(step);
 
   createEffect<MusicPlayedStore>((prev) => {
     const { data } = musicStore();
@@ -48,7 +64,11 @@ export default function MediaPlayer() {
         <Show when={song()}>{(song) => <SongPlayed song={song} />}</Show>
       </div>
 
-      <div class="justify-self-center">
+      <div class="flex gap-x-6 justify-self-center">
+        <button disabled={disabled()} class="-scale-x-100" onClick={() => stepSong(-1)}>
+          <NextIcon />
+        </button>
+
         <button
           class={`rounded-full h-10 w-10 grid place-content-center ${disabled() ? "bg-gray-500" : "bg-white"} text-black p-2`}
           onClick={toggle}
@@ -57,6 +77,10 @@ export default function MediaPlayer() {
           <Show when={playing()} fallback={<Play />}>
             <Pause />
           </Show>
+        </button>
+
+        <button disabled={disabled()} onClick={() => stepSong(1)}>
+          <NextIcon />
         </button>
       </div>
 
--- src/store/played.ts
+++ src/store/played.ts
@@ -5,14 +5,15 @@ export type MusicPlayedStore = {
   data: MusicPlayed | null;
   toggle: () => void;
   setPlaylist: (playlistId: string) => void;
+	stepSong: (step: number) => void;
 };
 
 export type MusicPlayed = {
   playlist: Playlist;
   musicId: number;
-	albumId: number;
+  albumId: number;
   isPlaying: boolean;
-	song: Song
+  song: Song;
 };
 
 export const useMusicPlayed = create<MusicPlayedStore>((set) => {
@@ -46,9 +47,24 @@ export const useMusicPlayed = create<MusicPlayedStore>((set) => {
           data: {
             playlist,
             musicId: music.id,
-						albumId: music.albumId,
+            albumId: music.albumId,
             isPlaying: true,
-						song: music
+            song: music,
+          },
+        };
+      }),
+
+    stepSong: (step: number) =>
+      set((state) => {
+        if (!state.data) return {};
+        const song = stepSong(state.data.musicId, step);
+        if (!song) return {};
+
+        return {
+          data: {
+            ...state.data,
+            musicId: song.id,
+            song,
           },
         };
       }),
@@ -61,4 +77,10 @@ function getPlaylist(playlistId: string): Playlist | undefined {
 
 function getMusic(albumId: number): Song | undefined {
   return songs.find((song) => song.albumId === albumId);
+}
+
+function stepSong(currentSongId: number, step: number): Song | undefined {
+  const index = songs.findIndex((song) => song.id === currentSongId);
+  const nextIndex = (index + step + songs.length) % songs.length;
+  return songs.at(nextIndex);
 }
