4acda9604ec799f9b0fdc05cc22a51bd3ff4208e
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-03-08 21:50:38 -0500

    feat: list songs of playlist

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				glass: {
+					DEFAULT: 'hsla(0, 0%, 100%, 0.1)',
+					dark: 'hsla(0, 0%, 0%, 0.3)',
+				}
+			}
+		},
 	},
 	plugins: [],
 }
